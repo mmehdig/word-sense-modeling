1bcaf08f945b83a16b024c7bdab69ff76bb41f72
Mehdi Ghanimifard <mmehdi.g@gmail.com>
2015-06-04 10:58:54 +0200

    clean up + add comments

--- src/python/combine_alignments.py
+++ src/python/combine_alignments.py
@@ -2,56 +2,72 @@
 
 __author__ = 'mehdi'
 
-import sys
+import sys, os
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 5:
         sys.exit('Please set raw src file, parallel file, and both reverse and forward aligns, :\n %s <src_raw_file> <parallel_file> <forward.align> <reverse.align>' % sys.argv[0])
 
-    src_file_name = sys.argv[1]
-    parallel_file_name = sys.argv[2]
-    forward_file_name = sys.argv[3]
-    reverse_file_name = sys.argv[4]
+    src_file_path = os.path.normpath(sys.argv[1])
+    parallel_file_path = os.path.normpath(sys.argv[2])
+    forward_file_path = os.path.normpath(sys.argv[3])
+    reverse_file_path = os.path.normpath(sys.argv[4])
 
-    with open(src_file_name) as en_file:
-        with open(parallel_file_name) as parallel_file:
-            with open(forward_file_name) as forward_file:
-                with open(reverse_file_name) as reverse_file:
-                    with open(parallel_file_name + "embed_align", "w") as out_file:
-                        for line in parallel_file:
-                            english = en_file.readline().strip().split(" ")
-                            swedish = line.strip().split(" ||| ")[1].split(" ")
-                            map_forward = forward_file.readline().strip().split(" ")
-                            map_reverse = reverse_file.readline().strip().split(" ")
+    src_file_path, src_file_name = os.path.split(src_file_path)
+    parallel_file_path, parallel_file_name = os.path.split(parallel_file_path)
+    forward_file_path, forward_file_name = os.path.split(forward_file_path)
+    reverse_file_path, reverse_file_name = os.path.split(reverse_file_path)
 
-                            # union of two different mappings
-                            # if any of the mappings had an empty string, just remove it from final union
-                            mappings = set(map_forward + map_reverse) - {''}
-                            sent_embed = dict()
-                            for mapping in mappings:
-                                i, j = tuple(int(x) for x in mapping.split("-"))
-                                if i in sent_embed:
-                                    sent_embed[i].append(swedish[j])
-                                else:
-                                    sent_embed[i] = [swedish[j]]
+    # open files:
+    en_file = open(os.path.join(src_file_path, src_file_name))
+    parallel_file = open(os.path.join(parallel_file_path, parallel_file_name))
+    forward_file = open(os.path.join(forward_file_path, forward_file_name))
+    reverse_file = open(os.path.join(reverse_file_path, forward_file_name))
 
-                            sentence = []
-                            for i, word in enumerate(english):
-                                emb = ""
-                                if i in sent_embed:
-                                    emb = "|".join(sent_embed[i])
-                                if emb:
-                                    sentence.append("%s__%s" % (word, emb))
-                                else:
-                                    sentence.append(word)
+    # output file:
+    out_file = open(os.path.join(src_file_path, src_file_name + ".out.augmented.txt"), "w")
 
-                            if sentence:
-                                out_file.write(" ".join(sentence) + "\n")
-                        out_file.close()
-                    reverse_file.close()
-                forward_file.close()
-            parallel_file.close()
-        en_file.close()
+    # for each line in parallel input-file of fast-align,
+    # split Swedish and English sentences.
+    # Then, in parallel read mapping outputs of fast-align,
+    # and for each word alignments in both reverse and forward alignment,
+    # create the augmented from the source file.
+    for line in parallel_file:
+        english = en_file.readline().strip().split(" ")
+        swedish = line.strip().split(" ||| ")[1].split(" ")
+        map_forward = forward_file.readline().strip().split(" ")
+        map_reverse = reverse_file.readline().strip().split(" ")
+
+        # union of two different mappings
+        # if any of the mappings had an empty string, just remove it from final union
+        mappings = set(map_forward + map_reverse) - {''}
+        sent_embed = dict()
+        for mapping in mappings:
+            i, j = tuple(int(x) for x in mapping.split("-"))
+            if i in sent_embed:
+                sent_embed[i].append(swedish[j])
+            else:
+                sent_embed[i] = set(swedish[j].split("|"))
+
+        sentence = []
+        for i, word in enumerate(english):
+            emb = ""
+            if i in sent_embed:
+                emb = "|".join(sent_embed[i])
+
+            if emb:
+                sentence.append("%s__%s" % (word, emb))
+            else:
+                sentence.append(word)
+
+        if sentence:
+            out_file.write(" ".join(sentence) + "\n")
+
+    out_file.close()
+    reverse_file.close()
+    forward_file.close()
+    parallel_file.close()
+    en_file.close()
 
 
