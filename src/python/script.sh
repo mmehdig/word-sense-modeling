f1d4e9763ca80eccc551ab2298e00a584728253c
Mehdi Ghanimifard <mmehdi.g@gmail.com>
2015-06-05 11:51:20 +0200

    preprocessing script

--- src/python/script.sh
+++ src/python/script.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-$INPUT1 = $1
-$INPUT2 = $2
+SRCDIR=`dirname $0`
+FAST_ALIGN_DIR="$SRCDIR/../../../fast_align"
 
-#$INPUT1 = "europarl-v7.sv-en.uniq.mixedpipeline_en.conll"
-#$INPUT2 = "europarl-v7_sv.lemmatized.txt"
+INPUT1="$1"
+INPUT2="$2"
+#$INPUT1="europarl-v7.sv-en.uniq.mixedpipeline_en.conll"
+#$INPUT2="europarl-v7_sv.lemmatized.txt"
+
 # First, process parallel sentences:
-python convert_to_text.py $INPUT1 $INPUT2
+python $SRCDIR/convert_to_text\.py $INPUT1 $INPUT2
 
 # Second, align words
 # executable fast_align (https://github.com/clab/fast_align)
-./fast_align \-i $INPUT1\_\_$INPUT2\.out.parallel.txt \-d \-o \-v > $INPUT1\_\_$INPUT2\.out.parallel.forward.align
-./fast_align \-i $INPUT1\_\_$INPUT2\.out.parallel.txt \-d \-o \-v \-r > $INPUT1\_\_$INPUT2\.out.parallel.reverse.align
-
+$FAST_ALIGN_DIR/fast_align \-i $INPUT1\_\_$INPUT2\.out\.parallel\.txt \-d \-o \-v > $INPUT1\_\_$INPUT2\.out\.parallel\.forward\.align
+$FAST_ALIGN_DIR/fast_align \-i $INPUT1\_\_$INPUT2\.out\.parallel\.txt \-d \-o \-v \-r > $INPUT1\_\_$INPUT2\.out\.parallel\.reverse\.align
 
 # Finally, create the final product:
-python combine_alignments.py $INPUT1\.out.raw.txt $INPUT1\_\_$INPUT2\.out.parallel.txt $INPUT1\_\_$INPUT2\.out.parallel.forward.align $INPUT1\_\_$INPUT2\.out.parallel.reverse.align
+python $SRCDIR/combine_alignments\.py $INPUT1\.out\.raw\.txt $INPUT1\_\_$INPUT2\.out\.parallel\.txt $INPUT1\_\_$INPUT2\.out\.parallel\.forward\.align $INPUT1\_\_$INPUT2\.out\.parallel\.reverse\.align
